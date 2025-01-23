6ba6dc40bd972157483aceda64b24da6feb4bc65
i-ally <insharah212@gmail.com>
2025-01-23 20:51:22 +0530

    third test

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,3 +19,16 @@ test("should return the sum of multiple numbers separated by commas", () => {
   expect(add("1,2,3,4,5")).toBe(15);
 });
 
+test("should handle new lines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+  expect(add("1\n2\n3")).toBe(6);
+});
+
+test("should support custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+  expect(add("//#\n2#3#4")).toBe(9);
+});
+
+test("should throw an exception for negative numbers", () => {
+  expect(() => add("-1,2,-3")).toThrow("negative numbers not allowed: -1, -3");
+});
