78f29ac5289a94f7ec64f7e5aa49fe0e97e2e2ac
i-ally <insharah212@gmail.com>
2025-01-23 20:39:17 +0530

    first test

---
+++ stringCalculator.test.js
@@ -0,0 +1,10 @@
+const add = require("./stringCalculator");
+
+test("should return 0 for an empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test("should return the number itself if input is a single number", () => {
+  expect(add("1")).toBe(1);
+  expect(add("5")).toBe(5);
+});
